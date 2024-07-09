7e5cb0b30e6b31a2b1b50879eb78ab5d570b1d1b
Shivam Mishra <scm.mymail@gmail.com>
2024-07-09 16:18:58 +0530

    fix: type issues

---
+++ src/utils/format.ts
@@ -0,0 +1,7 @@
+export function writeNumberNicely(num: number) {
+  return new Intl.NumberFormat("en-US", {
+    notation: "compact",
+    compactDisplay: "short",
+    maximumFractionDigits: 1,
+  }).format(num);
+}
